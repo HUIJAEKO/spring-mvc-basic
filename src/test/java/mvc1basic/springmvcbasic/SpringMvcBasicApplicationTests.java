224ca89d0ac6f0c2b294504eae40b019043d49d1
HUIJAEKO <xrhgmlwox@naver.com>
2024-09-27 13:41:14 +0900

    first commit

---
+++ src/test/java/mvc1basic/springmvcbasic/SpringMvcBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package mvc1basic.springmvcbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
